3e749477e4f304ac59ce382817b7765fe910768a
qu1xs <kevinnealmurphy@gmail.com>
2020-07-16 12:33:28 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  # get '/auth/github/callback' => 'sessions#create'
+  # post '/auth/github/callback' => 'sessions#create'
 
   root 'welcome#home'
 end
